37a90d980c27fa3977e37ba688c78b915b165242
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-13 01:15:05 +0530

    Added authController

---
+++ Getlinked-backend/src/controllers/authController.js
@@ -0,0 +1,90 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const generateAccessToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+};
+
+const generateRefreshToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, mobile, gender } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ msg: "All fields required" });
+    }
+    const normalizedEmail = email.toLowerCase();
+
+    const userExists = await User.findOne({ email: normalizedEmail });
+    if (userExists) return res.status(400).json({ msg: "User exists" });
+    if (password.length < 6) {
+      return res.status(400).json({ msg: "Passowrd is too short" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email: normalizedEmail,
+      password: hashedPassword,
+      mobile,
+      gender,
+    });
+
+    const accessToken = generateAccessToken(user._id);
+    const refreshToken = generateRefreshToken(user._id);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    const { password: _, ...userData } = user._doc;
+    res.json({ accessToken, user: userData });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ msg: "all fields are required" });
+    }
+
+    const normalizedEmail = email.toLowerCase();
+    const user = await User.findOne({ email: normalizedEmail });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid Email" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid creds" });
+    }
+
+    const accessToken = generateAccessToken(user._id);
+    const refreshToken = generateRefreshToken(user._id);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+    });
+    const { password: _, ...userData } = user._doc;
+    res.json({ accessToken, user: userData });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
