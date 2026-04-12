b26ce2d4400000f493b387aacf0963c10605fa2b
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-12 23:59:54 +0530

    second commit

---
+++ Getlinked-backend/src/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  mobile: String,
+  gender: String,
+  password: {
+    type: String,
+    required: true
+  },
+
+  bio: String,
+  skills: [String],
+
+  followers: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+  ],
+  following: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+  ]
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
