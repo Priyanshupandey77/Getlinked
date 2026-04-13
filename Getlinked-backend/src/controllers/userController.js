9b2631bf2a7ffba0cb61e15d9d353eacb4175718
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-13 21:31:41 +0530

    Follow / Unfollow System

--- Getlinked-backend/src/controllers/userController.js
+++ Getlinked-backend/src/controllers/userController.js
@@ -7,3 +7,52 @@ export const getMyProfile = async (req, res) => {
 
   res.json(user);
 };
+
+export const followUser = async (req, res) => {
+  const userToFollow = await User.findById(req.params.id);
+  const currentUser = await User.findById(req.user._id);
+
+  if (!userToFollow) {
+    return res.status(404).json({ msg: "User not found" });
+  }
+
+  if (userToFollow._id.equals(currentUser._id)) {
+    return res.status(400).json({ msg: "you cannot follow yourself" });
+  }
+  if (currentUser.following.some((id) => id.equals(userToFollow._id))) {
+    return res.status(400).json({ msg: "Already following" });
+  }
+
+  currentUser.following.push(userToFollow._id);
+  userToFollow.followers.push(currentUser._id);
+
+  await currentUser.save();
+  await userToFollow.save();
+
+  res.json({ msg: "User followed" });
+};
+
+export const unfollowUser = async (req, res) => {
+  const userToUnfollow = await User.findById(req.params.id);
+  const currentUser = await User.findById(req.user._id);
+
+  if (!userToUnfollow) {
+    return res.status(404).json({ msg: "User not found" });
+  }
+  if (!currentUser.following.some((id) => id.equals(userToUnfollow._id))) {
+    return res.status(400).json({ msg: "Not following this user" });
+  }
+
+  currentUser.following = currentUser.following.filter(
+    (id) => id.toString() !== userToUnfollow._id.toString(),
+  );
+
+  userToUnfollow.followers = userToUnfollow.followers.filter(
+    (id) => id.toString() !== currentUser._id.toString(),
+  );
+
+  await currentUser.save();
+  await userToUnfollow.save();
+
+  res.json({ msg: "User unfollowed" });
+};
--- Getlinked-backend/src/routes/userRoutes.js
+++ Getlinked-backend/src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getMyProfile } from "../controllers/userController.js";
+import { followUser, getMyProfile, unfollowUser } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/me", protect, getMyProfile);
+router.post("/follow/:id", protect, followUser);
+router.post("/unfollow/:id", protect, unfollowUser);
 
 export default router;
