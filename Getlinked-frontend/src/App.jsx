2ebc133ea699cc9140e80af4eda80c5e6c7e23f6
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-14 21:36:12 +0530

    Initial frontend setup

---
+++ Getlinked-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="text-2xl text-center mt-10">
+      DevConnect 🚀
+      <h1 className="text-3xl text-blue-500">Tailwind Working 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
