8128a566008d95352217d6bac5a6387f0f713093
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-16 18:44:19 +0530

    added frontend link in cors

--- Getlinked-backend/src/server.js
+++ Getlinked-backend/src/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://getlinked-two-psi.vercel.app",
     credentials: true,
   }),
 );
